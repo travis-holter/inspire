2c1b5cb0522d345e6388c6181bbbc016fec8e3b6
Travis Holter <travis.holter@intimetec.com>
2021-04-05 15:10:02 -0600

    remove unnecesary comments

--- app.js
+++ app.js
@@ -6,25 +6,12 @@ function updateClock() {
   var currentHours = currentTime.getHours();
   var currentMinutes = currentTime.getMinutes();
   var currentSeconds = currentTime.getSeconds();
-
-  // Pad the minutes and seconds with leading zeros, if required
   currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
   currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
-  // Choose either "AM" or "PM" as appropriate
   var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-
-  // Convert the hours component to 12-hour format if needed
   currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
-  // Convert an hours component of "0" to "12"
   currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-
-  // Compose the string for display
   var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-
-  // Update the time display
-//   document.getElementById("clock").firstChild.nodeValue = currentTimeString;
   document.getElementById("clock").innerHTML = currentTimeString;
 }
 
@@ -44,7 +31,6 @@ function getBackground() {
 const quote = document.getElementById('quote');
 const author = document.getElementById('author');
 
-
 function getQuote() {
     fetch('https://quotesondesign.com/wp-json/wp/v2/posts/?orderby=rand')
     .then(result => result.json())
@@ -53,6 +39,9 @@ function getQuote() {
         author.innerHTML = `<p>${response[0].title.rendered}</p>`
     })
 }
+
+// Weather stuff
+
 
 // Invoke stuff
 updateClock(); 
